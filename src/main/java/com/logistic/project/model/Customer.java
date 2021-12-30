5d226cc33019d6dc017cccf094f0506b3941a9c4
Mishamba <misha.nenahov@gmail.com>
2021-12-30 13:23:49 +0300

    Fixed controller endpoints
    Added annotations to Customer fields

--- src/main/java/com/logistic/project/model/Customer.java
+++ src/main/java/com/logistic/project/model/Customer.java
@@ -6,11 +6,15 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Document
 @AllArgsConstructor
 public class Customer {
     @Id
+    @NotNull
     private String name;
+    @NotNull
     private Coordinate position;
 }
