a33bc1731523763c145360a023f99101bc6565b2
Mishamba <misha.nenahov@gmail.com>
2022-01-03 11:51:01 +0300

    Added 2 endpoints for getting orders and warehouses
    Added OrderRepository
    Added save of Orders

--- src/main/java/com/logistic/project/controller/LogisticController.java
+++ src/main/java/com/logistic/project/controller/LogisticController.java
@@ -4,6 +4,8 @@ import com.logistic.project.model.Customer;
 import com.logistic.project.model.Order;
 import com.logistic.project.model.Warehouse;
 import com.logistic.project.model.dto.MakeOrderDTO;
+import com.logistic.project.repository.OrderRepository;
+import com.logistic.project.repository.WarehouseRepository;
 import com.logistic.project.repository.impl.CustomWarehouseRepositoryImpl;
 import com.logistic.project.repository.CustomerRepository;
 import com.logistic.project.util.CoordinateCalculator;
@@ -18,13 +20,25 @@ import java.util.List;
 public class LogisticController {
     private final CustomerRepository customerRepository;
     private final CustomWarehouseRepositoryImpl customWarehouseRepositoryImpl;
+    private final WarehouseRepository warehouseRepository;
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/orders")
+    public List<Order> findOrders(@RequestParam("customerName") String customerName) {
+        return orderRepository.findOrderByCustomerName(customerName);
+    }
+
+    @GetMapping("/warehouses")
+    public List<Warehouse> findWarehouses() {
+        return warehouseRepository.findAll();
+    }
 
     @PostMapping("/customer")
     public Customer saveNewCustomer(@RequestBody Customer customer) {
-        return customerRepository.save(customer);
+        return customerRepository.insert(customer);
     }
 
-    @PutMapping("/order")
+    @PostMapping("/order")
     public Order makeOrder(@RequestBody MakeOrderDTO makeOrderDTO) {
         Customer customer = customerRepository.findById(makeOrderDTO.getCustomerDTO().getName()).
                 orElseThrow(() -> new IllegalArgumentException("no such customer"));
@@ -43,6 +57,6 @@ public class LogisticController {
         finalOrder.setWarehouse(finalWarehouse);
         finalOrder.setDistance(CoordinateCalculator.getCoordinateDistance(customer.getPosition(), finalWarehouse.getPosition()));
 
-        return finalOrder;
+        return orderRepository.insert(finalOrder);
     }
 }
