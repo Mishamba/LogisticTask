e4eb96f79d3a2eed91ee3655d5e0939c19579323
Mishamba <misha.nenahov@gmail.com>
2022-01-11 18:02:29 +0300

    Tests refactor

--- src/test/java/com/logistic/project/util/CoordinateCalculatorTest.java
+++ src/test/java/com/logistic/project/util/CoordinateCalculatorTest.java
@@ -1,34 +1,31 @@
 package com.logistic.project.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.testcontainers.shaded.org.apache.commons.lang.ObjectUtils;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoordinateCalculatorTest {
 
-    @Test
-    void getCoordinateDistance() {
-        Coordinate coordinate1 = new Coordinate(0, 0);
-        Coordinate coordinate2 = new Coordinate(3, 4);
-        Coordinate coordinate3 = new Coordinate(1, 1);
-        Coordinate coordinate4 = new Coordinate(4, -3);
-        Coordinate coordinate5 = new Coordinate(1, 0);
-        Coordinate coordinate6 = new Coordinate(2, 0);
-        Coordinate coordinate7 = new Coordinate(10, 10);
-        Coordinate coordinate8 = new Coordinate(10, 15);
+    @ParameterizedTest
+    @MethodSource("provideCoordinatesAndDistances")
+    void getCoordinateDistance(Coordinate firstCoordinate, Coordinate secondCoordinate, double expectedDistance) {
+        double actualDistance = CoordinateCalculator.getCoordinateDistance(firstCoordinate, secondCoordinate);
+        assertEquals(expectedDistance, actualDistance);
+    }
 
-        double expected1_2_4 = 5.0;
-        double actual1 = CoordinateCalculator.getCoordinateDistance(coordinate1, coordinate2);
-        double actual2 = CoordinateCalculator.getCoordinateDistance(coordinate3, coordinate4);
-        double expected3 = 1.0;
-        double actual3 = CoordinateCalculator.getCoordinateDistance(coordinate5, coordinate6);
-        double actual4 = CoordinateCalculator.getCoordinateDistance(coordinate7, coordinate8);
-
-        assertEquals(expected1_2_4, actual1);
-        assertEquals(expected1_2_4, actual2);
-        assertEquals(expected3, actual3);
-        assertEquals(expected1_2_4, actual4);
+    private static Stream<Arguments> provideCoordinatesAndDistances() {
+        return Stream.of(
+                Arguments.of(new Coordinate(0, 0), new Coordinate(3, 4), 5.0),
+                Arguments.of(new Coordinate(1, 1), new Coordinate(4, -3), 5.0),
+                Arguments.of(new Coordinate(1, 0), new Coordinate(2, 0), 1.0),
+                Arguments.of(new Coordinate(10, 10), new Coordinate(10, 15), 5.0)
+        );
     }
 
     @Test
