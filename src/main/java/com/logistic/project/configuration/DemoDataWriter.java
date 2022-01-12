dbecf17cae17519d966bf8ab62d2972b02e149d3
Mishamba <misha.nenahov@gmail.com>
2022-01-12 12:18:17 +0300

    Fixed adding data to database after application started

--- src/main/java/com/logistic/project/configuration/DemoDataWriter.java
+++ src/main/java/com/logistic/project/configuration/DemoDataWriter.java
@@ -3,8 +3,11 @@ package com.logistic.project.configuration;
 import com.mongodb.MongoWriteException;
 import lombok.AllArgsConstructor;
 import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
@@ -20,8 +23,8 @@ public class DemoDataWriter implements ApplicationRunner {
 
     // TODO fix exception in docker container
     @Override
-    public void run(ApplicationArguments args) {
-        File folder = new File("resources/mongo");
+    public void run(ApplicationArguments args) throws IOException {
+        File folder = new ClassPathResource("mongo").getFile();
         File[] files = folder.listFiles(File::isFile);
         assert files != null;
         for (File file : files) {
